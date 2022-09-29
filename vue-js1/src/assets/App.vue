87f5ed8e732eac14d9d9ca556e873ea7df835da7
Gardene <gardene.v.c@gmail.com>
2022-09-29 20:35:17 -0300

    Projeto em vue

---
+++ vue-js1/src/assets/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <comments/>
+</template>
+
+<script>
+import { comments } from './components/comments'
+
+export default {
+  name: 'App', 
+  components: {
+    comments
+  }
+}
+</script>
+
+<style>
+
+
+</style>
